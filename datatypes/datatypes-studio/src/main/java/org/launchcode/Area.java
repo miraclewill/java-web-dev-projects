abda6ed7ee6379b4bd6d7d015fd8da1b1b6ac119
Miracle Williams <miraclewilliams.phila@gmail.com>
2023-08-21 20:53:09 -0400

    -finished studio

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,17 @@
+package org.launchcode;
+import studios.areaofacircle.Circle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+
+        double userRadius = input.nextDouble();
+
+        double areaOfCircle = Circle.getArea(userRadius);
+
+        System.out.println("The area of a circle of radius " + userRadius + " is: " + areaOfCircle);
+    }
+}
